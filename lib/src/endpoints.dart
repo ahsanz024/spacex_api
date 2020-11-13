b4fa83df254d861f086e4412b230a9b6c1870bee
Ahsan <ahsan024@gmail.com>
2020-11-14 00:25:22 +0500

    add: list of all available api endpoints

---
+++ lib/src/endpoints.dart
@@ -0,0 +1,17 @@
+/**
+ * List of all endpoints available in v4 of spacex-api
+ */
+class Endpoints {
+  static const capsules = "capsules";
+  static const company = "company";
+  static const crew = "crew";
+  static const dragons = "dragons";
+  static const history = "history";
+  static const launches = "launches";
+  static const landpads = "landpads";
+  static const payloads = "payloads";
+  static const roadster = "roadster";
+  static const rockets = "rockets";
+  static const ships = "ships";
+  static const starlink = "starlink";
+}
